4db9f303ba76bc4991b34fe32dc6d73462cffced
devhimanshuu <devhimanshuu@gmail.com>
2024-05-03 04:51:35 +0530

    BannerCard

---
+++ frontend/src/home/BannerCard.jsx
@@ -0,0 +1,43 @@
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import "./BannerCard.css";
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+const BannerCard = () => {
+  return (
+    <div className="banner pr-32">
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide></SwiperSlide>
+        <SwiperSlide></SwiperSlide>
+        <SwiperSlide></SwiperSlide>
+        <SwiperSlide></SwiperSlide>
+        <SwiperSlide></SwiperSlide>
+        <SwiperSlide></SwiperSlide>
+        <SwiperSlide></SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
+
+export default BannerCard;
